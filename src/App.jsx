58dd79ee9609ed9217a1ca1dbbdc6ce2ece42d9d
Abdul Waleed <abdulwaleedfsd@gmail.com>
2024-10-25 16:40:34 -0700

    complete navbar section

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,14 @@
 import React from "react";
+import Navbar from "./components/navbar/Navbar.jsx";
+import Hero from "./components/hero/Hero.jsx";
 
 const App = () => {
-  return <div></div>;
+  return (
+    <div className="app">
+      <Navbar />
+      <Hero />
+    </div>
+  );
 };
 
 export default App;
--- src/components/navbar/Navbar.jsx
+++ src/components/navbar/Navbar.jsx
@@ -1,8 +1,60 @@
-import React from "react";
+import React, { useState } from "react";
 import "./Navbar.css";
+import { FaArrowRightLong } from "react-icons/fa6";
+import { GiHamburgerMenu } from "react-icons/gi";
 
 const Navbar = () => {
-  return <div></div>;
+  const [menu, setMenu] = useState("home");
+  const [hamMenu, setHamMenu] = useState(false);
+
+  return (
+    <div className="navbar">
+      <h2 className="logo">Haseeb</h2>
+      <ul className="menu-items">
+        <a
+          href="#home"
+          onClick={() => setMenu("home")}
+          className={menu === "home" ? "active" : ""}
+        >
+          Home
+        </a>
+        <a
+          href="#about"
+          onClick={() => setMenu("about")}
+          className={menu === "about" ? "active" : ""}
+        >
+          About me
+        </a>
+        <a
+          href="#education"
+          onClick={() => setMenu("education")}
+          className={menu === "education" ? "active" : ""}
+        >
+          Education
+        </a>
+        <a
+          href="#experience"
+          onClick={() => setMenu("experience")}
+          className={menu === "experience" ? "active" : ""}
+        >
+          Experience
+        </a>
+        <a
+          href="#achievements"
+          onClick={() => setMenu("achievements")}
+          className={menu === "achievements" ? "active" : ""}
+        >
+          Achievements
+        </a>
+      </ul>
+
+      <a href="#contact" onClick={() => setMenu("contact")} className="contact">
+        Contact me
+        <FaArrowRightLong />
+      </a>
+      <GiHamburgerMenu className="ham-menu" />
+    </div>
+  );
 };
 
 export default Navbar;
